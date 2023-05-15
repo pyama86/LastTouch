991033d3cb4e9f4a65e6265fbe8df60da21e33fe
pyama <pyama@pepabo.com>
2023-05-15 17:26:43 +0900

    set host

--- misc/setup_test_db.sh
+++ misc/setup_test_db.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -x
 host=${MYSQL_HOST:-localhost}
-mysql -uroot -h $host -e "CREATE DATABASE IF NOT EXISTS  lasttouch"
-mysql -uroot -h $host -e "CREATE TABLE IF NOT EXISTS lasttouch.example (
+mysql -uroot -h "$host" -e "CREATE DATABASE IF NOT EXISTS  lasttouch"
+mysql -uroot -h "$host" -e "CREATE TABLE IF NOT EXISTS lasttouch.example (
     id INT(11) NOT NULL auto_increment PRIMARY KEY,
     message TEXT
 ) DEFAULT CHARSET=utf8";
-mysql -uroot -h $host -e "INSERT INTO lasttouch.example (message) VALUES ('Hello World')"
+mysql -uroot -h "$host" -e "INSERT INTO lasttouch.example (message) VALUES ('Hello World')"
